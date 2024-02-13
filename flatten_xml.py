0c3995109a81f43fe6b0dc2147b5edda6a3e204b
jerseyweeds <jfwiedmayer+github@gmail.com>
2024-02-13 14:05:48 -0500

    Update flatten_xml.py

--- flatten_xml.py
+++ flatten_xml.py
@@ -1,16 +1,25 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-def flatten_xml(elem, path='', tag_dict={}):
+def flatten_xml(elem, path='', tag_dict={}, index=0):
     """
     Recursively flatten the XML and handle conflicts by prepending parent tag names.
+    Also, handle elements with 'Size' attribute.
     """
-    if len(elem):
+    if 'Size' in elem.attrib:
+        size = int(elem.attrib['Size'])
+        child_tag = elem.tag
+        for i in range(size):
+            sub_path = f"{path}{child_tag}_{i+1}_"
+            for child in elem[i]:
+                flatten_xml(child, sub_path, tag_dict, i)
+    elif len(elem):
         path += elem.tag + '_'
         for child in elem:
-            flatten_xml(child, path, tag_dict)
+            flatten_xml(child, path, tag_dict, index)
     else:
-        tag_dict[path.rstrip('_')] = elem.text
+        tag_name = f"{path.rstrip('_')}_{index+1}" if index > 0 else path.rstrip('_')
+        tag_dict[tag_name] = elem.text
 
 def xml_to_dataframe(file_path):
     """
