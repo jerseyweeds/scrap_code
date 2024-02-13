2c153ed79480c732554d025fdd9e5de1767f0eb1
jerseyweeds <jfwiedmayer+github@gmail.com>
2024-02-13 14:44:02 -0500

    Update flatten_xml.py

--- flatten_xml.py
+++ flatten_xml.py
@@ -1,29 +1,27 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-def flatten_xml(elem, path='', tag_dict={}, index=0):
+def flatten_xml(elem, path='', tag_dict={}, parent_index=1, split_char='|'):
     """
-    Recursively flatten the XML and handle conflicts by prepending parent tag names.
-    Also, handle elements with 'Size' attribute.
+    Recursively flatten the XML and handle conflicts by prepending parent tag names with a custom delimiter.
+    Handle elements with 'Size' attribute by processing each child individually.
     """
     if 'Size' in elem.attrib:
         size = int(elem.attrib['Size'])
-        child_tag = elem.tag
-        for i in range(size):
-            sub_path = f"{path}{child_tag}_{i+1}_"
-            for child in elem[i]:
-                flatten_xml(child, sub_path, tag_dict, i)
+        for i, child in enumerate(elem, start=1):
+            new_path = f"{path}{elem.tag}{split_char}{i}{split_char}"
+            flatten_xml(child, new_path, tag_dict, split_char=split_char)
     elif len(elem):
-        path += elem.tag + '_'
+        new_path = f"{path}{elem.tag}{split_char}"
         for child in elem:
-            flatten_xml(child, path, tag_dict, index)
+            flatten_xml(child, new_path, tag_dict, split_char=split_char)
     else:
-        tag_name = f"{path.rstrip('_')}_{index+1}" if index > 0 else path.rstrip('_')
+        tag_name = f"{path.rstrip(split_char)}{split_char}{parent_index}" if parent_index > 1 else path.rstrip(split_char)
         tag_dict[tag_name] = elem.text
 
-def xml_to_dataframe(file_path):
+def xml_to_dataframe(file_path, split_char='|'):
     """
-    Convert XML data from a file to pandas DataFrame.
+    Convert XML data from a file to pandas DataFrame using a custom delimiter for tag names.
     """
     tree = ET.parse(file_path)
     root = tree.getroot()
@@ -31,7 +29,7 @@ def xml_to_dataframe(file_path):
     all_records = []
     for elem in root:
         record = {}
-        flatten_xml(elem, '', record)
+        flatten_xml(elem, '', record, split_char=split_char)
         all_records.append(record)
 
     return pd.DataFrame(all_records)
