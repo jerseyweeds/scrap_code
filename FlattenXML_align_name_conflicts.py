9542b4e96c15a3eb67f56da05e0fc64c3160cdad
jerseyweeds <jfwiedmayer+github@gmail.com>
2024-02-20 16:02:19 -0500

    Update FlattenXML_align_name_conflicts.py

--- FlattenXML_align_name_conflicts.py
+++ FlattenXML_align_name_conflicts.py
@@ -1,34 +1,40 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-def flatten_xml(elem, path='', tag_dict={}):
+def flatten_xml(elem, path='', row_data=None, all_data=None):
     """
-    Recursive function to flatten the XML and collect data in a dictionary
+    Recursive function to flatten the XML, handling nested structures.
     """
-    if list(elem):
-        path += elem.tag + '.'
-        for child in elem:
-            flatten_xml(child, path, tag_dict)
-    else:
+    if row_data is None:
+        row_data = {}
+    if all_data is None:
+        all_data = []
+
+    has_children = False
+    for child in elem:
+        has_children = True
+        child_path = f'{path}{elem.tag}.'
+        flatten_xml(child, child_path, row_data.copy(), all_data)
+
+    if not has_children:
         tag = path + elem.tag
-        if tag in tag_dict:
-            if type(tag_dict[tag]) is list:
-                tag_dict[tag].append(elem.text)
-            else:
-                tag_dict[tag] = [tag_dict[tag], elem.text]
-        else:
-            tag_dict[tag] = elem.text
-    return tag_dict
+        row_data[tag] = elem.text
+        all_data.append(row_data)
+
+    return all_data
 
 def xml_to_dataframe(xml_file):
     """
-    Converts XML file to pandas DataFrame
+    Converts XML file to pandas DataFrame, handling nested structures.
     """
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    
+
     # Flatten the XML and get data
-    data = [flatten_xml(child) for child in root]
+    data = []
+    for child in root:
+        flattened_data = flatten_xml(child)
+        data.extend(flattened_data)
 
     # Create DataFrame
     df = pd.DataFrame(data)
